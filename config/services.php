9e0d448d6b73bbd4656de1bb527a0b4fdaaef7c2
zorn <zorn84@bk.ru>
2016-11-18 12:32:04 +1000

    Updates for silex 2

--- config/services.php
+++ config/services.php
@@ -2,21 +2,15 @@
 
 use Symfony\Component\HttpFoundation\Session\Storage\Handler\PdoSessionHandler;
 
-$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
-
 $app->register(new Silex\Provider\TwigServiceProvider(), [
     'twig.path' => __DIR__ . '/../views',
-    'twig.form.templates' => ['share/form_layout.html.twig'],
     'twig.options' => [
         'cache' => __DIR__ . '/../var/cache/twig',
     ],
 ]);
-$app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
-    $twig->addFunction(new \Twig_SimpleFunction('asset', function ($asset) use ($app) {
-        return $app['request']->getBasePath().'/'.ltrim($asset, '/');
-    }));
-    return $twig;
-}));
+$app->register(new Silex\Provider\AssetServiceProvider(), [
+    'assets.version' => 'v1'
+]);
 
 $app->register(new Silex\Provider\SessionServiceProvider(), [
     'session.storage.save_path' => __DIR__ . '/../var/sessions'
@@ -29,9 +23,9 @@ $app->register(new Silex\Provider\SecurityServiceProvider(), [
             'anonymous' => true,
             'form' => ['login_path' => '/login', 'check_path' => '/login_check'],
             'logout' => ['logout_path' => '/logout', 'invalidate_session' => true],
-            'users' => $app->share(function () {
+            'users' => function () {
                 return new App\Security\UserProvider();
-            }),
+            },
         ]
     ]
 ]);
